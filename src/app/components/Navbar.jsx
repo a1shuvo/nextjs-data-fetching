ba5ac02e5bfb91e20a1776b303733c819d8b0678
a1shuvo <a1shuvosaha@gmail.com>
2025-07-22 21:25:15 +0600

    Install nextjs with tailwindcss, daisyui and implement Navbar component in the app

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-md px-4">
+      <div className="navbar-start">
+        <Link href="/" className="btn btn-ghost text-xl">
+          MyApp
+        </Link>
+      </div>
+
+      <div className="navbar-center">
+        <ul className="menu menu-horizontal px-1 space-x-2">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-end">
+        {/* Optional: Add login/user icon/cart/etc. */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
