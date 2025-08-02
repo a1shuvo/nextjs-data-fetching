8a31e2ce79356c6952acd192975c7ed6bb77e93b
a1shuvo <a1shuvosaha@gmail.com>
2025-08-02 20:02:44 +0600

    Add middleware to protect routes

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export const middleware = async (req) => {
+  const token = await getToken({ req });
+  const isTokenOK = Boolean(token);
+  const isAdminUser = token?.role === "admin";
+  const isAdminSpecificRoute = req.nextUrl.pathname.startsWith("/products");
+
+  if (isAdminSpecificRoute && !isAdminUser) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+  }
+
+  return NextResponse.next();
+};
